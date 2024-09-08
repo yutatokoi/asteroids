c6130d185971ce353cb317e17110fd38a9f16ae4
yutatokoi <yutatokoi@proton.me>
2024-09-08 20:58:37 +1000

    circleshape

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from constants import *
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,9 +16,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

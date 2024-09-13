c0c6a9b8ade0ead5c2751eeb20d453b61d5c5a18
yutatokoi <yutatokoi@proton.me>
2024-09-13 20:25:05 +1000

    rotate player with a and d keys

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()

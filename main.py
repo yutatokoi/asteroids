b5a9f7bb1f5e8e659bd49980b975dc92e5669dd1
yutatokoi <yutatokoi@proton.me>
2024-09-13 20:36:07 +1000

    add Players into sprite groups

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
@@ -18,9 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
